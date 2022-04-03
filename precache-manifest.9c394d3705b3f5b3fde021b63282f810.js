68dd81e2181f75c3eaa384a2410cffbebdadbf24
KOJEMYATKO <berteraskojemyatko@gmail.com>
2022-04-03 20:14:36 +0300

    Updates

---
+++ precache-manifest.9c394d3705b3f5b3fde021b63282f810.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e230d0d0fe7558d1337b5e3451585b48",
+    "url": "/3DHEAD/index.html"
+  },
+  {
+    "revision": "da1fcbd42c19bddcf755",
+    "url": "/3DHEAD/static/css/main.5a9b1316.chunk.css"
+  },
+  {
+    "revision": "049bb7d7c1f8d2d82a98",
+    "url": "/3DHEAD/static/js/2.9c518763.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/3DHEAD/static/js/2.9c518763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da1fcbd42c19bddcf755",
+    "url": "/3DHEAD/static/js/main.4cf58e0b.chunk.js"
+  },
+  {
+    "revision": "d94df30800b21208c358",
+    "url": "/3DHEAD/static/js/runtime-main.6d714647.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/3DHEAD/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/3DHEAD/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/3DHEAD/static/media/times-solid.2c5552a1.svg"
+  }
+]);
